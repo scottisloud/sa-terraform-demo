1edd9473a2ea803598d47622945df458bcea7039
Scott Lougheed <scott.lougheed@agilebits.com>
2022-01-26 20:24:02 +0000

    version bumps

--- terraform-demo/main.tf
+++ terraform-demo/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     onepassword = {
       source = "1Password/onepassword"
-      version = "~> 1.0.0"
+      version = "~> 1.1.4"
     }
   }
 }
@@ -82,6 +82,12 @@ resource "onepassword_item" "demo_sections" {
     field {
       label = "User"
       value = "demo"
+    }
+    
+    field {
+      label = "one-time password"
+      type = "OTP"
+      value = "NKD3YNEYD76CBAHILV4KTHGKUJWZ25ULUGETAB3ZINLZZDOVWATMYSD2HETDWVYE"
     }
   }
 }
